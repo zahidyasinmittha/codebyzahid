829dba5bb2daf0954e46235acbe5293470019a9a
zahidyasinmittha <b.zahidmittha@gmail.com>
2023-03-28 19:13:46 +0500

    all is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
-import {BrowserRouter} from 'react-router-dom';
-import {About , Contact , Experience , Feedbacks , Hero , Navbar , Tech , Works , StarsCanvas , Social} from './components';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Social } from "./components";
 
-const App =()=>{
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a delay for the page to load
+    const timer = setTimeout(() => {
+      setLoading(false);
+    });
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary overflow-hidden'>
-        <div className='bg-hero-pattern bg-cover bg-center bg-no-repeat'>
-          <Navbar />
-          <Hero />
+      {loading ? (
+        // Loading animation
+        <div>Loading...</div>
+      ) : (
+        // Page content
+        <div className="relative z-0 bg-primary overflow-hidden">
+          <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <Social />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <Social />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
+      )}
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
