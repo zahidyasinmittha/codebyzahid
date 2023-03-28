b284feb39c0dada80f326aea4e7e18c864d8ce45
zahidyasinmittha <b.zahidmittha@gmail.com>
2023-03-28 16:59:30 +0500

    all is done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../style";
-import { ComputersCanvas } from "./canvas";
+import { EarthCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -26,7 +26,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      <EarthCanvas />
       <div
         className="w-full absolute xs:bottom-10 bottom-3 flex
         justify-center items-center"
