8d760efb3cbab4e4a928bef447de48f115b825e1
zahidyasinmittha <b.zahidmittha@gmail.com>
2023-03-28 19:43:54 +0500

    all is done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
       </div>
       {!isLoading && <ComputersCanvas />}
       <div
-        className="w-full absolute xs:bottom-10 bottom-15 flex
+        className="w-full absolute xs:bottom-10 bottom-[10%] flex
         justify-center items-center"
       >
         <a href="#about">
