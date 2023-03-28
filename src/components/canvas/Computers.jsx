5c5ac3d7f0ff9ae55dc6d9b0fe52177b53818939
zahidyasinmittha <b.zahidmittha@gmail.com>
2023-03-28 17:21:32 +0500

    all is done

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,15 +4,11 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer =isMobile?useGLTF("./laptop/scene.gltf"):useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF(isMobile ? "./laptop/scene.gltf" : "./desktop_pc/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={isMobile ? 0.4 : 0.15} groundColor="black" />
-      {!isMobile ? (
-        <spotLight position={[25, 50, 10]} angle={0.15} intensity={1} />
-      ) : (
-        ""
-      )}
+      {!isMobile ? <spotLight position={[25, 50, 10]} angle={0.15} intensity={1} /> : ""}
       <pointLight intensity={0.3} />
       <primitive
         object={computer.scene}
@@ -23,47 +19,78 @@ const Computers = ({ isMobile }) => {
     </mesh>
   );
 };
+
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const mediaQuery = window.matchMedia("(max-width: 500px)");
     setIsMobile(mediaQuery.matches);
+
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
+
     mediaQuery.addEventListener("change", handleMediaQueryChange);
+
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
     <Canvas
-      // frameloop="demand"
+      frameloop="demand"
       dpr={[1, 2]}
-      camera={{ position:isMobile? [5, 5, 5]:[20, 3, 5], fov:isMobile ? 30:25,near: 0.1,
-        far: 30}}
-      gl={{ preserveDrawingBuffer: true ,
+      camera={{
+        position: isMobile ? [5, 5, 5] : [20, 3, 5],
+        fov: isMobile ? 30 : 25,
+        near: 0.1,
+        far: 30,
+      }}
+      gl={{
+        preserveDrawingBuffer: true,
         powerPreference: "high-performance",
         antialias: false,
-        }}
-      
-      style={isMobile ?{
-        width : "100vw",
-        height :"330px",
-        position: "absolute",
-        top: "65%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-      }: {}}
+      }}
+      style={
+        isMobile
+          ? {
+              width: "100vw",
+              height: "330px",
+              position: "absolute",
+              top: "65%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }
+          : {}
+      }
     >
-      <Suspense >
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} />
+        {!isMobile || !isLoading ? (
+          <Computers isMobile={isMobile} />
+        ) : (
+          <mesh>
+            <boxBufferGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="gray" />
+          </mesh>
+        )}
       </Suspense>
 
       <Preload all />
