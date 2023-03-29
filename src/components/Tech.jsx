d8c4dc88f61895adbf79c075cf97ed62be2d53c4
zahidyasinmittha <b.zahidmittha@gmail.com>
2023-03-29 14:16:33 +0500

    final

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -11,25 +11,12 @@ const Tech = () => {
     // Simulate loading time
     setTimeout(() => {
       setLoading(false);
-    },2900);
+    },3000);
   }, []);
 
   return (
     <>
-      {loading ? (
-        <motion.div
-          className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-white z-50"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <motion.div
-            animate={{ rotate: 360 }}
-            transition={{ duration: 1, repeat: Infinity, repeatType: "loop" }}
-            className="w-10 h-10 border-4 border-gray-400 rounded-full loader"
-          />
-        </motion.div>
-      ) : (
+      {!loading && (
         <div className="flex flex-row flex-wrap justify-center gap-10">
           {technologies.map((technology) => (
             <div className="w-28 h-28" key={technology.name}>
